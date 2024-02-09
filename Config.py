114911e84738bef7146e94e8dcc787d5a50094d5
Bergman Dmitry <16bdidima@gmail.com>
2024-02-09 18:34:42 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,7 +3,7 @@ MAINPAGEURL = 'https://vkplay.live'
 # Необходимо заменить '*' в url после '/', например '/dyadyastas'
 STREAMPAGEURL = f'{MAINPAGEURL}/*'
 
-# Для авторизации необходимо заполнить
+# Для авторизации необходимо заполнить (только для пользователя который не стримил)
 USERLOGIN = ""
 USERPASSWORD = ""
 USERNAME = ""
